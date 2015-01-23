115d8ef199f28e3186c7f23128e61ffb9e6c32ed
Marco Krage <marco@my-azur.de>
2015-01-23 09:53:27 +0100

    Update jquery.filterChildren.js

--- jquery.filterChildren.js
+++ jquery.filterChildren.js
@@ -1,19 +1,31 @@
 (function ($) {
-  $.fn.filterChildren = function (options) {
-    var settings = $.extend({
-      childSelector: "> *"
-    }, options);
+    $.fn.filterChildren = function (options) {
+        var settings = $.extend({
+            childSelector: "> *"
+        }, options);
 
-    $(this).each(function () {
-      var $thisList = $(this).addClass("datasource");
-      $thisList.wrap("<div class='filterChildren'></div>");
-      $('<input type="text" placeholder="Filter" class="search" />').keyup(function () {
-        var $thisInput = $(this);
-        $thisList.find(settings.childSelector).show().filter(function (index) {
-          return $(this).text().toLowerCase().indexOf($thisInput.val().toLowerCase()) === -1;
-        }).hide();
-      }).insertBefore($thisList);
-    });
-    return this;
-  };
+        $(this).each(function () {
+            var $thisInput;
+            var $this = $(this);//.addClass("datasource");
+            
+            $this.wrap("<div class='filterChildren'></div>");
+
+            if(settings.$filterInput instanceof $){
+              $thisInput = settings.$filterInput;//.keyup(filterFuntion);
+            }else{
+              $thisInput = $('<input type="text" placeholder="Filter" class="search" />').insertBefore($this);
+            }
+            
+            console.log($thisInput);
+
+            var filterFuntion = function () {
+              $this.find(settings.childSelector).show().filter(function (index) {
+                  return $(this).text().toLowerCase().indexOf($thisInput.val().toLowerCase()) === -1;
+              }).hide();
+            };
+            
+            $thisInput.keyup(filterFuntion);
+        });
+        return this;
+    };
 }(jQuery));
