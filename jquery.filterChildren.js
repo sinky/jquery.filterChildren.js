12cbfca2c44758f78e67ee51b8d52833f7494c40
Marco Krage <marco@my-azur.de>
2015-01-23 10:18:17 +0100

    Update jquery.filterChildren.js

--- jquery.filterChildren.js
+++ jquery.filterChildren.js
@@ -1,31 +1,30 @@
 (function ($) {
-    $.fn.filterChildren = function (options) {
-        var settings = $.extend({
-            childSelector: "> *"
-        }, options);
+  $.fn.filterChildren = function (options) {
+    var settings = $.extend({
+      childSelector: "> *",
+      filterClass: 'filtered'
+    }, options);
 
-        $(this).each(function () {
-            var $thisInput;
-            var $this = $(this);//.addClass("datasource");
-            
-            $this.wrap("<div class='filterChildren'></div>");
+    $(this).each(function () {
+      var $thisInput;
+      var $this = $(this);//.addClass("datasource");
+      
+      $this.wrap("<div class='filterChildren'></div>");
 
-            if(settings.$filterInput instanceof $){
-              $thisInput = settings.$filterInput;//.keyup(filterFuntion);
-            }else{
-              $thisInput = $('<input type="text" placeholder="Filter" class="search" />').insertBefore($this);
-            }
-            
-            console.log($thisInput);
+      if(settings.$filterInput instanceof $){
+        $thisInput = settings.$filterInput;//.keyup(filterFuntion);
+      }else{
+        $thisInput = $('<input type="text" placeholder="Filter" class="filter" />').insertBefore($this);
+      }
 
-            var filterFuntion = function () {
-              $this.find(settings.childSelector).show().filter(function (index) {
-                  return $(this).text().toLowerCase().indexOf($thisInput.val().toLowerCase()) === -1;
-              }).hide();
-            };
-            
-            $thisInput.keyup(filterFuntion);
-        });
-        return this;
-    };
+      var filterFuntion = function () {
+        $this.find(settings.childSelector).removeClass(settings.filterClass).filter(function (index) {
+          return $(this).text().toLowerCase().indexOf($thisInput.val().toLowerCase()) === -1;
+        }).addClass(settings.filterClass);
+      };
+      
+      $thisInput.keyup(filterFuntion);
+    });
+    return this;
+  };
 }(jQuery));
